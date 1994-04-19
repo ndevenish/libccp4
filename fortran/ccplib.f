13a0eb81b3b63a57fe61ddf25f50ab9bebd3f857
Dave Love <fx>
1994-04-19 15:14:01 +0000

    new header only

--- fortran/ccplib.f
+++ fortran/ccplib.f
@@ -2359,7 +2359,7 @@ C
       CALL UGTUID(UID)
       CALL UTIME(CTIME)
       WRITE (ILP,FMT=6000) PR,DT,UID(1:LENSTR(UID)),DT2,CTIME
- 6000 FORMAT ('1### CCP PROGRAM SUITE: ',A10,2X,'VERSION 2.3: ',
+ 6000 FORMAT ('1### CCP PROGRAM SUITE: ',A10,2X,'VERSION 2.4: ',
      +       A8,'###',/' User: ',A,'  Run date: ',A8,'  Run time:',A,
      +       /)
 C
