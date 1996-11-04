2ef5e75eea3891223de4cf62eaeadf687c349d2d
A R <acr>
1996-11-04 10:03:41 +0000

    machine dependent stuff moved to library.h.

--- chelp.h
+++ chelp.h
@@ -27,166 +27,8 @@
  *                                                                          *
  ****************************************************************************/
 
-/****************************************************************************
- * Known Machines                                                           *
- ****************************************************************************/
-
-/* Each type of system we know about should cause KNOWN_MACHINE to be
-   defined and also define CALL_LIKE_<something> to be defined.  Thus
-   if you know system foo has a Fortran calling convention like the
-   native Sun compiler uses, define CALL_LIKE_SUN and you won't need
-   to examine the definitions of the interface functions below.
-   Further tests on the system type may be necessary e.g., to get the
-   include files right. 
-
-   Note it's assumed below that a Fortran INTEGER corresponds to a C
-   int and a Fortran REAL corresponds to a C float.
-*/
-
-#if F2C				/* netlib f2c compiler -- not
-				   currently functional */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1	/* only for a single character
-				   variable in the arg list! */
-#else
-#if defined (_AIX)                        /* IBM unix - models RS/6000 */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_HPUX 1
-#endif
-#if defined (alliant)                          /* alliant model FX28xx */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#endif
-#if defined (ardent) || defined (titan)  /* (st)ardent and titan range */
-#  ifndef stardent
-#    define stardent
-#  endif
-#endif
-#if defined (__convex__)                            /* Convex C series */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#endif
-#if defined (ESV)                /* Evans & Sutherland ESV workstation */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#endif
-#if defined (__hpux)                         /* Hewlett Packard models */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_HPUX 1
-#endif
-#if defined (iris)                      /* Silicon graphics with 68xxx */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_IRIS 1
-#endif
-#ifdef __sgi			/* ansi */
-#  ifndef sgi
-#    define sgi
-#  endif
-#endif
-#if defined (sgi)                     /* Silicon graphics with R3000's */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#endif
-#if defined (solbourne)                                   /* sun clone */
-#  ifndef sun
-#   define sun               /* don't know whether it's defined or not */
-#  endif
-#endif
-#if defined (stardent)                                    /* as ardent */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_STARDENT 1
-#endif
-#if defined (sun)                                  /* sun workstations */
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#  define strerror(i) sys_errlist[i] /* k&r compiler doesn't have it */
-#endif
-#if defined (ultrix) || defined(__OSF1__) || defined(__osf__)
-#  define KNOWN_MACHINE
-#  define CALL_LIKE_SUN 1
-#endif
-#if defined (VMS)                      /* DEC VAX-VMS operating system */
-#  define KNOWN_MACHINE
-#endif
-#endif
-
-#if ! defined (KNOWN_MACHINE)
-  #error System type is not known -- see the Installation Guide
-#endif				/*  ! defined (KNOWN_MACHINE) */
-
-/****************************************************************************
- * Include Files                                                            *
- ****************************************************************************/
-#ifndef _POSIX_SOURCE
-#define _POSIX_SOURCE
-#endif
-
-#include <stdio.h>
-
-#if defined (VMS)
-#  include <descrip.h>		/* non-POSIX */
-#else
-#  include <sys/types.h>
-#  include <sys/times.h>
-#endif
-
-#if defined(NOUNISTD)		/* ESV, old Concentrix */
-#  include <sys/file.h>		/* non-POSIX */
-#else
-#  include <stdlib.h> 
-#endif
-
-#ifdef stardent			/* who knows if this works anyhow... */
-#  include <sys/types.h>
-#  include <malloc.h>		/* non-POSIX */
-#else
-#  include <stddef.h>
-#endif
-
-#include <string.h>		/* BSD might need strings.h and index
-				   instead of strchr.  This is ANSI
-				   header, not POSIX.1 */
-#if !defined(NOUNISTD) && !defined(VMS)
-#  include <unistd.h>
-#endif
-
-#include <errno.h>
-
-#ifdef _AIX			/* would do no harm on others, though */
-#  include <time.h>
-#endif
-
-#include <ctype.h>
-#include <curses.h> 
-
-/****************************************************************************
- * Defaults and Customisable items                                          *
- ****************************************************************************/
-
-#define MAXFLEN       500       /* the maximum length of a filename in CCP4 */
-#define MAXFILES       10    /* maximum number of files open symultaneously */
-#define FILE_FLUSH     20    /* force output buffer to flush every n writes */
-#define DEFSIZE         2    /* default mode access for random access files */
-
-#ifndef SEEK_SET                   /* Alliants don't have the file stdlib.h */
-#define SEEK_SET 0		   /* (now added in Concentrix3) */
-#define SEEK_CUR 1
-#define SEEK_END 2
-#endif /* ! SEEK_SET */
-
-#define IRRELEVANT_OP   0      /* used to show if an fseek is needed or not */
-#define READ_OP         1
-#define WRITE_OP        2
-
-/****************************************************************************
- * Error Numbers                                                            *
- ****************************************************************************/
-
-#define NO_ERR    0                                              /* success */
-#define NO_OPEN   1                                   /* error opening file */
-#define NO_CLOSE  2                                   /* error closing file */
-
-
+/* Library.h contains a lot of the machine dependent info. */
+#include "library.h"
 
 struct treenode *tree = NULL;   /* pointer to root node of tree structure */
 struct treenode *current = NULL;    /* used to point to current tree node */
@@ -200,15 +42,11 @@ int SCREEN_DEPTH ;
 
 
 
-
-
-
-
 /****************************************************************************
- * Machine dependent and byte swapping parts                                *
+ * Machine dependent and byte swapping parts; specific to chelp.            *
  ****************************************************************************/
 
-
+#include <curses.h>
 
 #ifdef CONVERT
 #  include "machine.h"
@@ -220,14 +58,6 @@ int SCREEN_DEPTH ;
 #  define DELFILE remove
 #else
 #  define DELFILE unlink
-#endif
-
-#if defined (ardent) || defined (titan) || defined (stardent)
-  struct Str_Desc {
-    char *Str_pointer;
-    int  Str_length;
-    int id;
-  };
 #endif
 
 #if defined (iris)		/* DL: i find this hard to believe... */
