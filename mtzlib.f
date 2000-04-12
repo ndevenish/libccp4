06d53dfbb3eafd3d93c39d557815a55ccc3638e7
Martyn Winn <martyn.winn@stfc.ac.uk>
2000-04-12 10:52:42 +0000

    Report cell to same precision as used in header.

--- mtzlib.f
+++ mtzlib.f
@@ -8646,7 +8646,7 @@ C
 C              ***********************
           CALL PUTLIN('* Cell Dimensions :','CURWIN')
           CALL BLANK('CURWIN',1)
-          WRITE (STROUT,FMT='(6F9.3)') (CELL(JJ,MINDX),JJ=1,6)
+          WRITE (STROUT,FMT='(6F10.4)') (CELL(JJ,MINDX),JJ=1,6)
           CALL PUTLIN(STROUT,'CURWIN')
           CALL BLANK('CURWIN',1)
 C              ***********************
