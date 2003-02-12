8b87c39df20e88422aaeed64338c8d0c95917530
Peter Briggs <pjx>
2003-02-12 12:49:08 +0000

    Added error codes for general functions.

--- ccplib.h
+++ ccplib.h
@@ -41,6 +41,27 @@ extern "C" {
 #define CCP4_MAXNAMES 150
 #define CCP4_MODULO   100000
 
+/* stuff for error reporting */
+#define CGEN_ERRNO(n) (CCP4_ERR_GEN | (n))
+
+/* error defs */
+#define  CGENERR_Ok                  0
+#define  CGENERR_AllocFail           1
+#define  CGENERR_CantSetEnvironment  2
+#define  CGENERR_MaxNamesExceeded    3
+#define  CGENERR_EOptionUseError     4
+#define  CGENERR_DOptionUseError     5
+#define  CGENERR_LogicalNameUseError 6
+#define  CGENERR_CantOpenEnvFile     7
+#define  CGENERR_CantOpenDefFile     8
+#define  CGENERR_ParseEnvFail        9
+#define  CGENERR_ParseDefFail        10
+#define  CGENERR_CantFindInFile      11
+#define  CGENERR_EnvPathFail         12
+#define  CGENERR_DefPathFail         13
+#define  CGENERR_CantGetClibd        14
+#define  CGENERR_CantGetCcp4Scr      15
+
 /*------------------------------------------------------------------*/
 
 /* Structures and typedefs */
