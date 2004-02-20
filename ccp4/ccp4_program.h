4f77f017c03a89c82e5b1f0cf2db5e40b14749dd
Peter Briggs <pjx>
2004-02-20 10:43:16 +0000

    Updated version to "5.0.beta1".

--- ccp4/ccp4_program.h
+++ ccp4/ccp4_program.h
@@ -65,7 +65,7 @@ extern "C" {
 #endif
 
 #define CCP4_VERSION_NO "5.0"
-#define CCP4_PATCH_LEVEL "5.0.beta"
+#define CCP4_PATCH_LEVEL "5.0.beta1"
 
 /* Maximum lengths of strings holding program names and versions */
 #define MAXLEN_PROGNAME    80
