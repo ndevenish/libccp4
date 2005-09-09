aa48e266d7a91ea750a36ed9de8d327163d46c99
Martyn Winn <martyn.winn@stfc.ac.uk>
2005-09-09 08:24:23 +0000

    cocked-up banners

--- fortran/dna_output.f
+++ fortran/dna_output.f
@@ -2,11 +2,10 @@ C
 C     dna_output.f: write output for the DNA project
 C     Copyright (C) 2004  CCLRC, Graeme Winter
 C
-     This code is distributed under the terms and conditions of the
-     CCP4 Program Suite Licence Agreement as a CCP4 Library.
-     A copy of the CCP4 licence can be obtained by writing to the
-     CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
-C
+C    This code is distributed under the terms and conditions of the
+C    CCP4 Program Suite Licence Agreement as a CCP4 Library.
+C    A copy of the CCP4 licence can be obtained by writing to the
+C    CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
 C
 c     dna_output.f
 c     maintained by G.Winter
