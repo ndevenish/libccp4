9dba04daa2294517ae1325ba27cf9cb9de012e95
Charles Ballard <charles.ballard@stfc.ac.uk>
2007-12-17 09:59:26 +0000

    update for test release

--- ccp4/ccp4_program.h
+++ ccp4/ccp4_program.h
@@ -31,7 +31,7 @@ extern "C" {
 #endif
 
 #define CCP4_VERSION_NO "6.0"
-#define CCP4_PATCH_LEVEL "6.0.0"
+#define CCP4_PATCH_LEVEL "6.0.99a"
 
 /* Maximum lengths of strings holding program names and versions */
 #define MAXLEN_PROGNAME    80
