6796dcd03751bbaa81c0827ab76eb07349b77ad9
Charles Ballard <charles.ballard@stfc.ac.uk>
2008-03-10 10:27:08 +0000

    update for 6.0.99c

--- ccp4/ccp4_program.h
+++ ccp4/ccp4_program.h
@@ -30,8 +30,8 @@ namespace CCP4 {
 extern "C" {
 #endif
 
-#define CCP4_VERSION_NO "6.0.99b"
-#define CCP4_PATCH_LEVEL "6.0.99b"
+#define CCP4_VERSION_NO "6.0.99c"
+#define CCP4_PATCH_LEVEL "6.0.99c"
 
 /* Maximum lengths of strings holding program names and versions */
 #define MAXLEN_PROGNAME    80
