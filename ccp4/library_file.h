f642813a1fbe41bd25e1483c3282fae8be39f874
Martyn Winn <martyn.winn@stfc.ac.uk>
2008-06-09 10:30:58 +0000

    update banner for modified LGPL v3

--- ccp4/library_file.h
+++ ccp4/library_file.h
@@ -2,10 +2,19 @@
      library_file.h: header file for library_file.c
      Copyright (C) 2001  CCLRC, Charles Ballard
 
-     This code is distributed under the terms and conditions of the
-     CCP4 Program Suite Licence Agreement as a CCP4 Library.
-     A copy of the CCP4 licence can be obtained by writing to the
-     CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+     This library is free software: you can redistribute it and/or
+     modify it under the terms of the GNU Lesser General Public License
+     version 3, modified in accordance with the provisions of the 
+     license to address the requirements of UK law.
+ 
+     You should have received a copy of the modified GNU Lesser General 
+     Public License along with this library.  If not, copies may be 
+     downloaded from http://www.ccp4.ac.uk/ccp4license.php
+ 
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU Lesser General Public License for more details.
 */
 
 /** @file library_file.h
