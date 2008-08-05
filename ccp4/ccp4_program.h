925a1917b347b43b7c1d5430376fe98576c8a034
Charles Ballard <charles.ballard@stfc.ac.uk>
2008-08-05 11:25:46 +0000

    merge in 6.0.99e changes

--- ccp4/ccp4_program.h
+++ ccp4/ccp4_program.h
@@ -39,8 +39,8 @@ namespace CCP4 {
 extern "C" {
 #endif
 
-#define CCP4_VERSION_NO "6.0.99d"
-#define CCP4_PATCH_LEVEL "6.0.99d"
+#define CCP4_VERSION_NO "6.0.99e"
+#define CCP4_PATCH_LEVEL "6.0.99e"
 
 /* Maximum lengths of strings holding program names and versions */
 #define MAXLEN_PROGNAME    80
