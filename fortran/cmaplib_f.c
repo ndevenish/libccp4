8569692ddc9b1ecb89bb5acf1bd6d30ff650d84a
Martyn Winn <martyn.winn@stfc.ac.uk>
2010-07-15 14:09:24 +0000

    Use %ld for "long" file size, see #4203

--- fortran/cmaplib_f.c
+++ fortran/cmaplib_f.c
@@ -88,7 +88,7 @@ static int ioArrayPrint(IOConvMap *ioMap)
   if (rw_mode == 1) {
     fprintf(stdout,"\nFile name for input map file on unit %3d : %s\n",
       ioMap->ipc,filename);
-    fprintf(stdout,"file size %d ; logical name %s\n\n",length,ioMap->logname);
+    fprintf(stdout,"file size %ld ; logical name %s\n\n",length,ioMap->logname);
   } else {
     fprintf(stdout,"\nFile name for output map file on unit %3d : %s\n",
       ioMap->ipc,filename);
@@ -187,7 +187,7 @@ static int HeaderPrint(const CMMFile *mfile)
           mfile->origin[2]+mfile->map_dim[2] - 1);
   fprintf(stdout,"           Grid sampling on x, y, z ........................%5d%5d%5d\n",
           mfile->cell_grid[0], mfile->cell_grid[1], mfile->cell_grid[2]);
-  fprintf(stdout,"           Cell dimensions .................................%10.5f%10.5f%10.5f%10.5f%10.5f%10.5f\n",
+  fprintf(stdout,"           Cell dimensions .................................%10.4f %10.4f %10.4f %10.4f %10.4f %10.4f\n",
           mfile->cell[0], mfile->cell[1], mfile->cell[2],
           mfile->cell[3], mfile->cell[4], mfile->cell[5]);
   fprintf(stdout,"           Fast, medium, slow axes .........................    %c    %c    %c\n",
